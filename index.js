ea16738a5a204b5c4bf0bc3d3dcdc9a5bd4124d3
Jesus-11Watts <113714568+Jesus-11Watts@users.noreply.github.com>
2022-12-11 12:37:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const cambio = document.getElementById("menu");
+
+cambio.addEventListener("click",()=>{
+    document.querySelector(".menu").classList.toggle("menu-grande");
+})
+
+const cambio2 = document.getElementById("menu-2");
+
+cambio2.addEventListener("click",()=>{
+    document.querySelector(".menu-2").classList.toggle("menu-grande");
+})
+
+const cambio3 = document.getElementById("menu-3");
+
+cambio3.addEventListener("click",()=>{
+    document.querySelector(".menu-3").classList.toggle("menu-grande");
+})
+
+const cambio4 = document.getElementById("menu-4");
+
+cambio4.addEventListener("click",()=>{
+    document.querySelector(".menu-4").classList.toggle("menu-grande");
+})
+
+const cambio5 = document.getElementById("menu-5");
+
+cambio5.addEventListener("click",()=>{
+    document.querySelector(".menu-5").classList.toggle("menu-grande");
+})
+
